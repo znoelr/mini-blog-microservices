25a5672fb87b8dca19c7ffd9324e97e889bddcde
znoelr <rennleonz@gmail.com>
2024-03-11 18:19:17 -0500

    Update post route to avoid ingress route override conflict

--- posts/index.js
+++ posts/index.js
@@ -10,11 +10,12 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
+app.all("*", (req, res) => {
+  const { url, baseUrl, originalUrl } = req;
+  res.json({ url, baseUrl, originalUrl });
 });
 
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
